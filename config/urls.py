1577f0c4715820cfa5cf14c001cd165a683e60e4
GabrielLima2803 <gabriellima2803@gmail.com>
2023-09-06 15:30:54 -0300

    Projeto Livraria Django

---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from livraria.views import CategoriaViewSet, EditoraViewSet, LivroViewSet, AutorViewSet
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"livros", LivroViewSet)
+router.register(r"autores", AutorViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+]
